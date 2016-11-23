f96fd61af2709f64f4adf4f3989127b2d93fc3ac
andela-nekekwe <ngozi.ekekwe@andela.com>
2016-11-23 18:17:04 +0100

    add comments to inverted-index.js

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,61 +1,82 @@
-class index{
-    constructor(){
-      this.fileMap = {};
+/**
+ * Index class
+ * @class
+ */
+class index {
+  /**
+   * class constructor
+   * @constructor
+   */
+  constructor() {
+    this.fileMap = {};
+  }
+  /**
+   * create index
+   * @function
+   * @param {Array} fileContent objects in an Array
+   * @return {Object} index object
+   */
+  createIndex(fileContent) {
+    this.docCount = [];
+    for (const object in fileContent) {
+      this.docCount.push(parseInt(object, 10));
     }
-    
-    //create index
-    createIndex(fileContent){
-        this.docCount = [];
-        for(const object in fileContent){
-            //explain this
-            this.docCount.push(parseInt(object,10));
-        };
-        fileContent.forEach((item, index)=>{
-            let content = `${item.title} ${item.text}`;
-            const token = this.tokenize(content);
-            token.forEach((item)=>{  
-                if(item in this.fileMap){
-                    this.fileMap[item].push(index);
-                }else{
-                    this.fileMap[item] =[];
-                    this.fileMap[item].push(index);
-                }
-            });
-        });
-        return this.fileMap;
+    fileContent.forEach((item, docTag) => {
+      const content = `${item.title} ${item.text}`;
+      const token = this.tokenize(content);
+      token.forEach((item) => {
+        if (item in this.fileMap) {
+          this.fileMap[item].push(docTag);
+        } else {
+          this.fileMap[item] = [];
+          this.fileMap[item].push(docTag);
         }
-
-    //get index
-    getIndex(){
-        return this.fileMap;
+      });
+    });
+    return this.fileMap;
+  }
+  /**
+   * Get index
+   * @function
+   * @return {Object} index object
+   */
+  getIndex() {
+    return this.fileMap;
+  }
+  /**
+   * Search Index
+   * @function
+   * @param {string} query string being searched
+   * @return {Object} search result is returned
+   */
+  searchIndex(query) {
+    const result = {};
+    if (query === undefined) {
+      return this.fileMap;
     }
-
-    //search index
-    searchIndex(query){
-        const result = {};
-        const search = query.split(' ');
-        search.forEach((word) =>{
-            if (this.fileMap.hasOwnProperty(word)){
-                result[word] = this.fileMap[word];
-            }
-        });
-
-        return Object.keys(result).length>0? result : 'Search Query Not Found';   
-    }
-
-    //tokenize
-    tokenize(text){
-        let unique =[];
-        let token = text.toLowerCase().replace(/[^\w\s]/gi, '').match(/\w+/g);
-        token.forEach((item)=>{
-            if(!unique.includes(item)){
-                unique.push(item);
-            }
-        });
-        return unique;
-    }
-
-    
+    const search = query.split(' ');
+    search.forEach((word) => {
+      if (this.fileMap.hasOwnProperty(word)) {
+        result[word] = this.fileMap[word];
+      }
+    });
+    return Object.keys(result).length > 0 ? result : 'Search Query Not Found';
+  }
+  /**
+   * removes special characters, white spaces and duplicates
+   * @function
+   * @param {string} text document title and text
+   * @return {Array} tokens
+   */
+  tokenize(text) {
+    const unique = [];
+    const token = text.toLowerCase().replace(/[^\w\s]/gi, '').match(/\w+/g);
+    token.forEach((item) => {
+      if (!unique.includes(item)) {
+        unique.push(item);
+      }
+    });
+    return unique;
+  }
 }
-
-// module.exports = index;
+module.exports = index;
